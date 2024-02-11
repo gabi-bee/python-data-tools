d94e857abf6b0e068bb0e3d62d71e4d3168c5a0a
gmabon <gabimabon@gmail.com>
2024-02-11 01:21:24 +0000

    Added df_col_checks.py and test_df_col_checks.py

---
+++ helpers/df_col_checks.py
@@ -0,0 +1,128 @@
+import re
+from collections.abc import Callable
+
+import numpy as np
+import pandas as pd
+
+from utils.df_utils import get_dtype_cols_dict, reshape_with_expected_cols
+
+# columns expected in final results
+expected_columns: dict[str, type] = {  # TODO update
+    'dtype_py_inferred': object,
+    'int_leading_zeros': int,
+    'str_len_min': int,
+    'str_len_max': int,
+    'str_len_checksum': int,
+}
+
+
+# main functions -------------------------------------------------------------------------------------------------------
+def run_suite_df_col_checks(df_inferred: pd.DataFrame, df_str: pd.DataFrame) -> pd.DataFrame:
+    """
+    runs checks on
+    :param df_inferred: python inferred dtype df
+    :param df_str: str forced dtype df
+    :return: dataframe containing results from checks
+    """
+    results: list[pd.DataFrame | pd.Series] = []
+
+    # define checks to run for each col dtype
+    dtype_checks_dict: dict[str, list[Callable]] = {  # TODO update
+        'object': [check_str_len_min_max_checksum],
+        'int64': [check_int_leading_zeros],
+        'float64': [],
+    }
+
+    # run checks on all cols
+    all_col_checks_df_inferred: list[Callable] = [check_all_dtypes, lambda x: [x.describe(include='all').transpose()]]
+    results += run_checks_on_cols(df_inferred, *all_col_checks_df_inferred, all_columns=True)
+
+    # run checks for each dtype cols (python inferred)
+    dtypes_dict: dict[str, list[str]] = get_dtype_cols_dict(df_inferred, expected_keys=list(dtype_checks_dict.keys()))
+    for dtype, dtype_checks in dtype_checks_dict.items():
+        results += run_checks_on_cols(df_str, *dtype_checks, columns=dtypes_dict[dtype])
+
+    # TODO re-run checks for each dtype cols (logic inferred)
+    # TODO run checks on final col
+
+    # prepare results df
+    df_results = pd.concat(results, axis=1)  # concatenate results into df
+    df_results = reshape_with_expected_cols(df_results, list(expected_columns.keys()))
+
+    return df_results
+
+
+# helper functions -----------------------------------------------------------------------------------------------------
+def run_checks_on_cols(df: pd.DataFrame, *checks: Callable[[pd.DataFrame], list[pd.Series]],
+                       columns: list[str] = None, all_columns: bool = False) -> list[pd.Series | pd.DataFrame]:
+    """
+
+    :param df:
+    :param checks:
+    :param columns:
+    :param all_columns:
+    :return:
+    """
+    results = []
+
+    if columns:
+        df_to_check = df[columns]
+        if all_columns:
+            print('warning: all_columns = True AND columns passed as args. checks will be run on just columns passed')
+    elif all_columns:
+        df_to_check = df
+    else:
+        df_to_check = None
+        print("warning: neither all_columns nor columns specified - therefore, checks aren't being run")
+
+    if columns or all_columns:
+        for check in checks:
+            check_result = check(df_to_check)  # run check(s)
+            results += check_result
+
+    return results
+
+
+# util functions -------------------------------------------------------------------------------------------------------
+def match_leading_zeros(x: str):
+    if pd.isna(x):
+        return np.nan
+    elif re.match('^0[0-9]+', x):
+        return 1
+    else:
+        return 0
+
+
+def get_str_len(x: str):
+    if pd.isna(x):
+        return np.nan
+    else:
+        return len(x)
+
+
+# individual check functions -------------------------------------------------------------------------------------------
+def check_all_dtypes(df_inferred: pd.DataFrame) -> list[pd.Series]:
+    dtypes = df_inferred.dtypes
+    dtypes.name = 'dtype_py_inferred'
+    return [dtypes]
+
+
+def check_int_leading_zeros(df_str: pd.DataFrame) -> list[pd.Series]:
+    int_leading_zeros = df_str.map(match_leading_zeros).sum()
+    int_leading_zeros.name = 'int_leading_zeros'
+    return [int_leading_zeros]
+
+
+def check_str_len_min_max_checksum(df_str: pd.DataFrame) -> list[pd.Series]:
+    df_lens = df_str.map(get_str_len)
+
+    str_len_min = df_lens.min()
+    str_len_min.name = 'str_len_min'
+
+    str_len_max = df_lens.max()
+    str_len_max.name = 'str_len_max'
+
+    str_len_checksum = df_lens.sum()
+    str_len_checksum.name = 'str_len_checksum'
+
+    return [str_len_min, str_len_max, str_len_checksum]
---
+++ tests/test_df_col_checks.py
@@ -0,0 +1,77 @@
+import unittest
+
+from helpers.df_col_checks import *
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+
+class DfChecksTests(unittest.TestCase):
+    _dict_to_df_examples = {
+        'ex_inf_int1': {'int1': [0, 1]},
+        'ex_str_int1': {'int1': ['0', '1']},
+        'ex_str_int1_leading0s': {'int1': ['0', '01']},
+    }
+    dfs = {key: pd.DataFrame.from_dict(value) for key, value in _dict_to_df_examples.items()}
+
+    # test main functions ----------------------------------------------------------------------------------------------
+    def test_run_suite_df_col_checks(self):
+        """test run_suite_df_col_checks fn"""
+
+        test_cases: list[dict[str, any]] = [
+            {'args': {'df_inferred': pd.DataFrame.from_dict({'col1': [0, 0], 'col2': [1, 1]}),
+                      'df_str': pd.DataFrame.from_dict({'col1': ['0', '0'], 'col2': ['1', '1']})}, 'returns': None},
+            {'args': {'df_inferred': self.dfs['ex_inf_int1'], 'df_str': self.dfs['ex_str_int1']}, 'returns': None},
+            {'args': {'df_inferred': self.dfs['ex_str_int1'], 'df_str': self.dfs['ex_str_int1']}, 'returns': None},
+            {'args': {'df_inferred': self.dfs['ex_inf_int1'], 'df_str': self.dfs['ex_str_int1_leading0s']},
+             'returns': None},
+
+        ]
+
+        # test return equals expected
+        for i, case in enumerate(test_cases):
+            print(f'\n\ncase: {i}')
+            print(f'\ndf_inferred: \n{case["args"]["df_inferred"]}')
+            print(f'\ndf_str: \n{case["args"]["df_str"]}')
+            result: pd.DataFrame = run_suite_df_col_checks(**case['args'])
+            # expected_result = case['returns']
+            print(f'\nresult: \n{result}')
+
+    # test helper functions --------------------------------------------------------------------------------------------
+    def test_match_leading_zeros(self):
+        """test match_leading_zeros fn"""
+        test_cases: list[dict[str, any]] = [
+            {'args': {'x': '1'}, 'returns': 0},
+            {'args': {'x': '01'}, 'returns': 1},
+            {'args': {'x': np.nan}, 'returns': np.nan},
+        ]
+
+        # test return equals expected
+        for case in test_cases:
+            result = match_leading_zeros(**case['args'])
+            expected_result = case['returns']
+            if pd.isna(expected_result):
+                self.assertTrue(pd.isna(result))
+            else:
+                self.assertEqual(result, expected_result)
+
+    # test individual check functions ----------------------------------------------------------------------------------
+    def test_check_int_leading_zeros(self):
+        """test check_int_leading_zeros fn"""
+
+        test_cases: list[dict[str, any]] = [
+            {'args': {'df_str': self.dfs['ex_str_int1']}, 'returns': pd.Series(data={'int1': 0})},
+            {'args': {'df_str': self.dfs['ex_str_int1_leading0s']}, 'returns': pd.Series(data={'int1': 1})},
+        ]
+
+        # test return equals expected
+        for case in test_cases:
+            result: pd.Series = check_int_leading_zeros(**case['args'])[0]
+            expected_result = case['returns']
+
+            self.assertTrue(result.equals(expected_result))
+            self.assertEqual(result.name, 'int_leading_zeros')
+
+
+if __name__ == '__main__':
+    unittest.main()
